ae375cc0357fb43a4489b1a8a9eb3f59818b410d
jimb-cc <jim@jimb.cc>
2020-06-18 00:11:53 +0100

    Made to infinte loop removed prog bar added debug.

--- lb_up.rb
+++ lb_up.rb
@@ -3,15 +3,14 @@
 require 'mongo'
 require 'faker'
 require 'slop'
-require 'progress_bar'
 
 opts = Slop.parse do |o|
   o.string '-h', '--host', 'the connection string for the MongoDB cluster (default: localhost)', default: 'mongodb://localhost'
   o.string '-d', '--database', 'the database to use (default: leaderboard)', default: 'leaderboard'
   o.string '-c', '--collection', 'the collection to use (default: lb)', default: 'lb'
   o.integer '-m', '--maxscore', 'the highest possible score on the leaderboard (default: 1,000,000)', default: 1_000_000
-  o.integer '-r', '--records', 'the number of records to generate (default: 10,000)', default: 500_000
   o.integer '-f', '--maxfriends', 'The maximum amount of friends a player can have', default: 50
+  o.integer '-q', '--debugfreq', 'the number of updates to complete before reporting debug', default: 1000
 end
 
 max_friends = 20
@@ -22,25 +21,31 @@ puts "Connecting to #{opts[:host]}, and db #{opts[:database]}"
 DB = Mongo::Client.new(opts[:host])
 DB.use(opts[:database])
 
-bar = ProgressBar.new(opts[:records])
-
-def makeDoc(db, coll, num_docs, max_score, max_friends, bar)
-  (1..num_docs.to_i).each do |_i|
-    friends = []
-    (1..(rand(max_friends) + 1)).each do
-      friends << "#{Faker::Esport.player}#{Faker::Beer.hop}"
-    end
-
-    result = db[coll].update_one({  'displayName' => "#{Faker::Esport.player}#{Faker::Beer.hop}#{[:"", :s, :er].sample}",
-                                    'level' => Faker::Cosmere.shard.to_s,
-                                    'platform' => Faker::Game.platform },
-                                 {
-                                   '$set' => { 'score' => rand(max_score), 'last_updated' => Time.now, 'friends' => friends },
-                                   '$inc' => { 'update_count': 1 }
-                                 },
-                                 upsert: true)
-    bar.increment!
+def makeDoc(db, coll, max_score, max_friends)
+  friends = []
+  (1..(rand(max_friends) + 1)).each do
+    friends << "#{Faker::Esport.player}#{Faker::Beer.hop}"
   end
+
+  result = db[coll].update_one({  'displayName' => "#{Faker::Esport.player}#{Faker::Beer.hop}#{[:"", :s, :er].sample}",
+                                  'level' => Faker::Cosmere.shard.to_s,
+                                  'platform' => Faker::Game.platform },
+                               {
+                                 '$set' => { 'score' => rand(max_score), 'last_updated' => Time.now, 'friends' => friends },
+                                 '$inc' => { 'update_count': 1 }
+                               },
+                               upsert: true)
 end
 
-makeDoc(DB, opts[:collection], opts[:records], opts[:maxscore], opts[:maxfriends], bar)
+puts 'Connected....'
+
+i = 0
+t = Time.now
+loop do
+  makeDoc(DB, opts[:collection], opts[:maxscore], opts[:maxfriends])
+  i += 1
+  if i % opts[:debugfreq] == 0
+    puts "Performed another #{opts[:debugfreq]} updates in #{Time.now - t} for a total of #{i}"
+    t = Time.now
+  end
+end
